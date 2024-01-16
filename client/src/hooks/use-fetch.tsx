5e97fb2929178fb61fd8ccf4386b723359334f7a
Andrey Ponomarev <info@andreyponomarev.ru>
2024-01-16 17:17:31 +0300

    fix: Rewrite for React 18 + get rid of useIsMounted hook

---
+++ client/src/hooks/use-fetch.tsx
@@ -0,0 +1,138 @@
+import React from "react";
+
+import { parseResponse } from "../utils/parse-response";
+import { handleResponseErr } from "../utils/handle-response-err";
+import { APIResponse } from "../types";
+
+type FetchInit = { type: "FETCH_INIT" };
+type FetchSuccess<T> = {
+  type: "FETCH_SUCCESS";
+  payload: APIResponse<T>["response"];
+};
+type FetchFailure<T> = {
+  type: "FETCH_FAILURE";
+  error: APIResponse<T>["error"] | Error;
+};
+type ResetState = { type: "RESET_STATE" };
+
+type Action<T> = FetchInit | FetchSuccess<T> | FetchFailure<T> | ResetState;
+type State<T> = {
+  isLoading: boolean;
+  error: null | Error | APIResponse<T>["error"];
+  response: null | APIResponse<T>["response"];
+};
+
+// We need this wrapping function only to pass the data type to the reducer
+function createDataFetchReducer<T>() {
+  // This is is the actual reducer function
+  return function (state: State<T>, action: Action<T>): State<T> {
+    switch (action.type) {
+      case "FETCH_INIT": {
+        return { ...state, isLoading: true, error: null };
+      }
+      case "FETCH_SUCCESS": {
+        return {
+          ...state,
+          isLoading: false,
+          error: null,
+          response: action.payload,
+        };
+      }
+      case "FETCH_FAILURE": {
+        return { ...state, isLoading: false, error: action.error };
+      }
+      case "RESET_STATE": {
+        return { isLoading: false, error: null, response: null };
+      }
+      default: {
+        throw new Error(`Unknown action`);
+      }
+    }
+  };
+}
+
+function useFetch<ResponseBody>(
+  url: RequestInfo,
+  request?: RequestInit
+): {
+  state: APIResponse<ResponseBody>;
+  resetState: () => void;
+} {
+  const initialState: State<ResponseBody> = {
+    isLoading: false,
+    error: null,
+    response: null,
+  };
+
+  const dataFetchReducer = createDataFetchReducer<ResponseBody>();
+
+  const [state, dispatch] = React.useReducer(dataFetchReducer, initialState);
+
+  function resetState() {
+    dispatch({ type: "RESET_STATE" });
+  }
+
+  //
+
+  let res: Response;
+
+  React.useEffect(() => {
+    dispatch({ type: "FETCH_INIT" });
+    console.log("[useFetch] Dispatched 'FETCH_INIT'");
+
+    const abortController = new AbortController();
+    const signal = abortController.signal;
+
+    (async function performFetching() {
+      try {
+        res = await fetch(url, { ...request, signal });
+      } catch {
+        // if component is not unmounted
+        if (!signal.aborted) {
+          dispatch({
+            type: "FETCH_FAILURE",
+            error: new Error(
+              "Something went wrong. Please check your connection."
+            ),
+          });
+          console.log("[useFetch] Dispatched 'FETCH_FAILURE'");
+        }
+        // If the request was aborted by the cleanup function (i.e. component
+        // was unmounted), then stop executing function:
+        return;
+      }
+
+      try {
+        const resBody = await parseResponse<ResponseBody>(res);
+        console.log("[useFetch] Response body:", resBody);
+
+        // if component is not unmounted
+        if (!signal.aborted) {
+          dispatch({ type: "FETCH_SUCCESS", payload: resBody });
+          console.log("[useFetch] Dispatched 'FETCH_SUCCESS'");
+        }
+      } catch (err) {
+        // if component is not unmounted
+        if (!signal.aborted) {
+          const parsedErr = await handleResponseErr(err as Response);
+          dispatch({
+            type: "FETCH_FAILURE",
+            error: parsedErr,
+          });
+          console.log("[useFetch] Dispatched 'FETCH_FAILURE'");
+        }
+      }
+    })();
+
+    // Return the cleanup function
+    return () => {
+      // Cancel the fetch request when the component unmounts
+      abortController.abort();
+    };
+    // empty array means "run 'useEffect' only once, after the initial render"
+  }, [url]);
+
+  return { state: state as APIResponse<ResponseBody>, resetState };
+}
+
+export { State, useFetch };
