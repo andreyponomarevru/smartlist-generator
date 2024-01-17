cc40358602755a6e2b467d76b1a80456d50b716c
Andrey Ponomarev <info@andreyponomarev.ru>
2024-01-17 20:07:20 +0300

    refactor: Replace div with li

--- client/src/lib/track/track.tsx
+++ client/src/lib/track/track.tsx
@@ -9,13 +9,13 @@ interface TrackProps {
 
 function Track(props: TrackProps) {
   return (
-    <div className="track">
+    <li className="track">
       <span className="track__year">{props.meta.year}</span>
       <span className="track__artist">{props.meta.artist}</span>
       <span className="track__title">{props.meta.title}</span>
       <span className="track__playback">Play buttons</span>
       <span className="track__length">{props.meta.length}</span>
-    </div>
+    </li>
   );
 }
 
