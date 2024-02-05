80bb9519981f32118529d469590aeb4f5c6b833d
Andrey Ponomarev <info@andreyponomarev.ru>
2024-02-05 20:36:50 +0300

    feat: Add new design

--- client/src/lib/group/group.tsx
+++ client/src/lib/group/group.tsx
@@ -8,12 +8,23 @@ import {
   useWatch,
   UseFormResetField,
   UseFormUnregister,
+  Controller,
 } from "react-hook-form";
+import Select, { StylesConfig } from "react-select";
 import uniqWith from "lodash/uniqWith";
 import mergeWith from "lodash/mergeWith";
 import { Btn } from "../btn/btn";
-import { Stats, FormValues, TrackMeta } from "../../types";
+import { Stats, FormValues, TrackMeta, OptionsList } from "../../types";
 import { Player } from "../player/player";
+import {
+  FILTER_NAMES,
+  GENRE_CONDITIONS,
+  YEAR_CONDITIONS,
+  OPERATORS,
+  DEFAULT_INPUTS,
+} from "../../config/constants";
+import { ConditionSelect } from "./condition-select";
+import { ValueSelect } from "./value-select";
 
 import "./group.scss";
 
@@ -29,138 +40,25 @@ interface GroupProps extends React.HTMLAttributes<HTMLDivElement> {
   handleAddGroup: () => void;
   handleDeleteGroup: () => void;
 }
-type SelectProps<T> = {
-  name: string;
-  control: Control<FormValues>;
-  index: number;
-  register: UseFormRegister<FormValues>;
-  resetField: UseFormResetField<FormValues>;
-  optionsList: T;
-  unregister?: UseFormUnregister<FormValues>;
-};
-type OptionsList = { [key: string]: { name: string; value: string } };
-const OPERATORS: OptionsList = {
-  or: { name: "any", value: "or" },
-  and: { name: "all", value: "and" },
-};
-const FILTER_NAMES: OptionsList = {
-  year: { name: "Year", value: "year" },
-  genre: { name: "Genre", value: "genre" },
-  any: { name: "Any", value: "any" },
-  all: { name: "All", value: "all" },
-};
-const GENRE_CONDITIONS: OptionsList = {
-  contains: { name: "contains", value: "contains" },
-  notcontains: { name: "does not contain", value: "does not contain" },
-};
-const YEAR_CONDITIONS: OptionsList = {
-  is: { name: "is", value: "is" },
-  isNot: { name: "is not", value: "is not" },
-  gte: { name: "greater than or equal", value: "greater than or equal" },
-  lte: { name: "less than or equal", value: "less than or equal" },
-};
-
-function ConditionSelect(props: SelectProps<OptionsList>) {
-  const defaultValue = Object.values(props.optionsList)[0].value;
-  const filtersWatch = useWatch({ name: "filters", control: props.control });
-
-  React.useEffect(() => {
-    if (filtersWatch[props.index]?.name === props.name) {
-      props.resetField(`filters.${props.index}.condition`, { defaultValue });
-    }
-  }, [filtersWatch[props.index]?.name]);
-
-  return filtersWatch[props.index]?.name === props.name ? (
-    <select
-      {...props.register(`filters.${props.index}.condition`)}
-      defaultValue={defaultValue}
-    >
-      {Object.values(props.optionsList).map((el) => (
-        <option key={el.value} value={el.value} children={el.name} />
-      ))}
-    </select>
-  ) : null;
-}
-
-function ValueSelect(props: SelectProps<Stats[]>) {
-  const defaultValue = props.optionsList[0].id || props.optionsList[0].name;
-  const filtersWatch = useWatch({ name: "filters", control: props.control });
-
-  React.useEffect(() => {
-    if (filtersWatch[props.index]?.name === props.name) {
-      props.resetField(`filters.${props.index}.value`, { defaultValue });
-    }
-  }, [filtersWatch[props.index]?.name]);
-
-  return filtersWatch[props.index]?.name === props.name ? (
-    <select
-      {...props.register(`filters.${props.index}.value`)}
-      defaultValue={defaultValue}
-    >
-      {props.optionsList.map((el) => (
-        <option key={el.id || el.name} value={el.id || el.name}>
-          {el.name}
-        </option>
-      ))}
-    </select>
-  ) : null;
-}
-
-function FilterSelect(props: SelectProps<OptionsList>) {
-  const defaultValue = props.optionsList.year.name;
-  const filtersWatch = useWatch({ name: "filters", control: props.control });
-
-  React.useEffect(() => {
-    if (
-      filtersWatch[props.index]?.name === "any" ||
-      filtersWatch[props.index]?.name === "all"
-    ) {
-      if (props.unregister) {
-        props.unregister(`filters.${props.index}.value`);
-        props.unregister(`filters.${props.index}.condition`);
-      }
-    }
-  }, [filtersWatch[props.index]?.name]);
-
-  return (
-    <>
-      <select
-        {...props.register(`filters.${props.index}.name`)}
-        defaultValue={defaultValue}
-      >
-        {...Object.values(FILTER_NAMES).map((el) => (
-          <option key={el.value} value={el.value}>
-            {el.name}
-          </option>
-        ))}
-      </select>
-
-      {filtersWatch[props.index]?.name === "any" ||
-      filtersWatch[props.index]?.name === "all"
-        ? "of the following are true"
-        : null}
-    </>
-  );
-}
 
 let renderCount = 0;
 
 export function Group(props: GroupProps) {
-  const genres = [...props.stats.genres].sort((a, b) =>
-    a.name.localeCompare(b.name)
-  );
-  const years = [...props.stats.years]
-    .map((y) => ({ ...y, name: parseInt(y.name) }))
-    .sort((a, b) => b.name - a.name)
-    .map((y) => ({ ...y, name: String(y.name) }));
+  const genres: OptionsList<number>[] = [...props.stats.genres]
+    .sort((a, b) => a.name.localeCompare(b.name))
+    .map((g) => ({ value: g.id as number, label: g.name }));
 
-  const defaultValues = {
-    operator: "and",
+  const years: OptionsList<number>[] = [...props.stats.years]
+    .sort((a, b) => parseInt(b.name) - parseInt(a.name))
+    .map((y) => ({ value: parseInt(y.name), label: String(y.name) }));
+
+  const defaultValues: FormValues = {
+    operator: OPERATORS[0],
     filters: [
       {
-        name: FILTER_NAMES.genre.value,
-        condition: GENRE_CONDITIONS.contains.value,
-        value: String(genres[0].id),
+        name: DEFAULT_INPUTS.filterName,
+        condition: DEFAULT_INPUTS.genre.condition,
+        value: genres[0],
       },
     ],
   };
@@ -222,13 +120,20 @@ export function Group(props: GroupProps) {
         >
           <div className="group__row">
             <label htmlFor="filter-form">Match</label>
-            <select {...register("operator")}>
-              {Object.values(OPERATORS).map((o) => (
-                <option key={o.value} value={o.value}>
-                  {o.name}
-                </option>
-              ))}
-            </select>
+
+            <Controller
+              name="operator"
+              control={control}
+              render={({ field }) => {
+                return (
+                  <Select
+                    {...field}
+                    options={OPERATORS}
+                    defaultValue={OPERATORS[0]}
+                  />
+                );
+              }}
+            />
             <div>of the following rules:</div>
           </div>
 
@@ -236,14 +141,18 @@ export function Group(props: GroupProps) {
             return (
               <div key={filter.id} className="group__row group__row_filter">
                 <div className="group__row-controls">
-                  <FilterSelect
-                    name="name"
+                  <Controller
+                    name={`filters.${index}.name`}
                     control={control}
-                    register={register}
-                    unregister={unregister}
-                    index={index}
-                    resetField={resetField}
-                    optionsList={FILTER_NAMES}
+                    render={({ field }) => {
+                      return (
+                        <Select
+                          {...field}
+                          options={FILTER_NAMES}
+                          defaultValue={DEFAULT_INPUTS.filterName}
+                        />
+                      );
+                    }}
                   />
                   <ConditionSelect
                     name="genre"
@@ -251,7 +160,9 @@ export function Group(props: GroupProps) {
                     register={register}
                     index={index}
                     resetField={resetField}
-                    optionsList={GENRE_CONDITIONS}
+                    options={GENRE_CONDITIONS}
+                    defaultValue={DEFAULT_INPUTS.genre.condition}
+                    setValue={setValue}
                   />
                   <ConditionSelect
                     name="year"
@@ -259,15 +170,20 @@ export function Group(props: GroupProps) {
                     register={register}
                     index={index}
                     resetField={resetField}
-                    optionsList={YEAR_CONDITIONS}
+                    options={YEAR_CONDITIONS}
+                    defaultValue={DEFAULT_INPUTS.year.condition}
+                    setValue={setValue}
                   />
+
                   <ValueSelect
                     name="genre"
                     control={control}
                     register={register}
                     index={index}
                     resetField={resetField}
-                    optionsList={genres}
+                    options={genres}
+                    defaultValue={genres[0]}
+                    setValue={setValue}
                   />
                   <ValueSelect
                     name="year"
@@ -275,7 +191,9 @@ export function Group(props: GroupProps) {
                     register={register}
                     index={index}
                     resetField={resetField}
-                    optionsList={years}
+                    options={years}
+                    defaultValue={years[0]}
+                    setValue={setValue}
                   />
                 </div>
 
@@ -283,7 +201,7 @@ export function Group(props: GroupProps) {
                   <button
                     type="button"
                     onClick={() =>
-                      insert(index + 1, [{ name: FILTER_NAMES.genre.value }])
+                      insert(index + 1, [{ name: FILTER_NAMES[1] }])
                     }
                     className="btn btn_theme_transparent-black group__btn"
                   >
@@ -319,6 +237,7 @@ export function Group(props: GroupProps) {
             return (
               <Player
                 {...track}
+                key={track.trackId}
                 isPlaying={props.isPlaying}
                 onPlayBtnClick={props.onPlayBtnClick}
               />
@@ -333,68 +252,3 @@ export function Group(props: GroupProps) {
     </div>
   );
 }
-
-const arr = [
-  {
-    name: "genre",
-    rule: "contains",
-    value: 29,
-  },
-  {
-    name: "genre",
-    rule: "does not contain",
-    value: 4,
-  },
-  {
-    name: "genre",
-    rule: "contains",
-    value: 18,
-  },
-  {
-    name: "year",
-    rule: "greater than or equal",
-    value: 1996,
-  },
-  {
-    name: "year",
-    rule: "is",
-    value: 2023,
-  },
-  {
-    name: "year",
-    rule: "greater than or equal",
-    value: 2001,
-  },
-  {
-    name: "year",
-    rule: "is",
-    value: 2017,
-  },
-];
-
-function buildQuery(
-  arr: {
-    name: string;
-    rule: string;
-    value: number | string;
-  }[]
-) {
-  const withUniqueYearRules = uniqWith(arr, (a, b) => {
-    if (a.name === "year") return a.name === b.name && a.rule === b.rule;
-    else return false;
-  });
-
-  const withAggregatedGenres = uniqWith(withUniqueYearRules, (a, b) => {
-    if (a.name === "genre") {
-      if (a.rule === b.rule) {
-        a.value = b.value = [].concat(a.value as any, b.value as any) as any;
-        return true;
-      } else return false;
-    } else return true;
-  });
-  console.log("!!!!!!!", withAggregatedGenres);
-
-  return withAggregatedGenres;
-}
-
-buildQuery(arr);
