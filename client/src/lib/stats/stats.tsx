14b336322f62328e1c06f45dfa3ca5223ef614ab
Andrey Ponomarev <info@andreyponomarev.ru>
2024-02-09 22:09:58 +0300

    refactor

--- client/src/lib/stats/stats.tsx
+++ client/src/lib/stats/stats.tsx
@@ -1,43 +1,38 @@
 import * as React from "react";
-import { GetStatsResponse, APIResponse } from "../../types";
+import { Stats, APIResponse, GetStatsRes } from "../../types";
 import { Message } from "../message/message";
 import { Loader } from "../loader/loader";
 
 import "./stats.scss";
 
 interface Props extends React.HTMLAttributes<HTMLElement> {
-  stats: APIResponse<GetStatsResponse>;
-  title: string;
+  stats: APIResponse<GetStatsRes>;
 }
 
 export function Stats(props: Props) {
   return (
     <section className={`stats ${props.className || ""}`}>
       <header className="stats__header">
-        <span className="stats__title">{props.title}</span>
-        <span className="stats__header-count">
-          {props.stats.response?.body?.results.length || 0}
-        </span>
+        <span>{props.title}</span>
+        <span>{props.stats.response?.body?.results.length || 0}</span>
       </header>
 
       {props.stats.isLoading && <Loader for="page" color="pink" />}
+
       {props.stats.error && (
         <Message type="danger">Something went wrong :(</Message>
       )}
 
       <ul className="stats__list">
-        {props.stats.response?.body && (
-          <>
-            {...props.stats.response.body?.results.map((entry) => {
-              return (
-                <li key={entry.id} className="stats__row">
-                  <span>{entry.name}</span>
-                  <span className="stats__counter">{entry.count}</span>
-                </li>
-              );
-            })}
-          </>
-        )}
+        {props.stats.response?.body?.results &&
+          props.stats.response?.body?.results.map((entry) => {
+            return (
+              <li key={entry.id || entry.name} className="stats__row">
+                <span>{entry.name}</span>
+                <span>{entry.count}</span>
+              </li>
+            );
+          })}
       </ul>
     </section>
   );
