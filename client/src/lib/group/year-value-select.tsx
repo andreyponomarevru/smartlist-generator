7c765a232c63af8376a16e36051a39edaeaee114
Andrey Ponomarev <info@andreyponomarev.ru>
2024-02-10 23:33:00 +0300

    feat: Add inputs

---
+++ client/src/lib/group/year-value-select.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { useWatch, Controller } from "react-hook-form";
+import Select from "react-select";
+import { SelectProps } from "../../types";
+
+export function YearValueSelect(props: SelectProps<number>) {
+  const filtersWatch = useWatch({ name: "filters", control: props.control });
+
+  React.useEffect(() => {
+    if (filtersWatch[props.index]?.name.value === props.name) {
+      props.resetField(`filters.${props.index}.value`, {
+        defaultValue: props.defaultValue,
+      });
+    }
+  }, [filtersWatch[props.index]?.name.value]);
+
+  // If the  name of this component (props.name) matches the filter name selected in dropdowb
+  return filtersWatch[props.index]?.name.value === props.name ? (
+    <Controller
+      name={`filters.${props.index}.value`}
+      control={props.control}
+      render={({ field }) => {
+        return (
+          <Select
+            {...field}
+            closeMenuOnSelect={true}
+            isMulti={false}
+            options={props.options}
+            defaultValue={props.defaultValue}
+          />
+        );
+      }}
+    />
+  ) : null;
+}
