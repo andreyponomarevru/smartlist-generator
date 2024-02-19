5f3025a079b27be0cacfa6ed844f5f898f5d38d0
Andrey Ponomarev <info@andreyponomarev.ru>
2024-02-19 13:36:55 +0300

    feat: Add message component

---
+++ client/src/lib/message/message.tsx
@@ -0,0 +1,15 @@
+import React, { PropsWithChildren, ReactElement } from "react";
+
+import "./message.scss";
+
+interface Props extends React.HTMLAttributes<HTMLSpanElement> {
+  type: "warning" | "success" | "info" | "danger" | "info" | "disabled";
+}
+
+export function Message(props: PropsWithChildren<Props>): ReactElement {
+  return (
+    <span className={`message message_${props.type} ${props.className || ""}`}>
+      {props.children}
+    </span>
+  );
+}
