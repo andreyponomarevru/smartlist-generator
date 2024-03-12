2206f72113ce039ca4f4e30e4baac5626c743355
Andrey Ponomarev <info@andreyponomarev.ru>
2024-03-12 11:49:43 +0300

    format: Add trailing commas

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -6,10 +6,10 @@ import { App } from "./app/app";
 import "./reset.scss";
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById("root") as HTMLElement,
 );
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
