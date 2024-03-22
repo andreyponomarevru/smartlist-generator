bf21ce248bc9cd9f75c4af7c918a14108e29913b
Andrey Ponomarev <info@andreyponomarev.ru>
2024-03-22 21:11:15 +0300

    feat: Add middleware

---
+++ api/src/middlewares/is-libpath-exist.ts
@@ -0,0 +1,22 @@
+import fs from "fs-extra";
+
+import { Request, Response, NextFunction } from "express";
+
+import { HttpError } from "../utils/error";
+
+export async function isLibPathExist(
+  req: Request<unknown, unknown, { libPath: string }>,
+  res: Response,
+  next: NextFunction,
+) {
+  if (req.body.libPath && (await fs.exists(req.body.libPath))) {
+    next();
+  } else {
+    next(
+      new HttpError({
+        code: 400,
+        message: "Incorrect lib path. Such dir doesn't exist",
+      }),
+    );
+  }
+}
