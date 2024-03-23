90dfed5c50ab9f98a5a11f141d46034e704b8d82
Andrey Ponomarev <info@andreyponomarev.ru>
2024-03-23 22:04:00 +0300

    feat: Add tested validation controller

--- api/src/controller/lib/validations.ts
+++ api/src/controller/lib/validations.ts
@@ -2,17 +2,22 @@ import { ChildProcess, SpawnOptions, spawn } from "child_process";
 
 import { Request, Response, NextFunction } from "express";
 
+import { NODE_ENV } from "../../config/env";
 import * as validationModel from "../../model/validation/index";
+import { ValidationResult } from "../../types";
+import { HttpError } from "../../utils/error";
 
 // We need to keep the job in memory in order to be able to kill it later
 let job: ChildProcess | null = null;
-const VALIDATION_JOB_OPTS: SpawnOptions = {
+const VALIDATION_OPTS: SpawnOptions = {
   detached: false,
   stdio: ["inherit", "inherit", "inherit", "ipc"],
 };
+const VALIDATION_SCRIPT_PATH = `./src/jobs/validate.${NODE_ENV === "development" ? "ts" : "js"}`;
+const NODE_PROCESS = NODE_ENV === "development" ? "ts-node" : "node";
 
 export async function runValidation(
-  req: Request<any, any, { libPath: string }>,
+  req: Request<unknown, unknown, { libPath: string }>,
   res: Response,
   next: NextFunction,
 ) {
@@ -21,25 +26,25 @@ export async function runValidation(
       res.status(500).json({ message: "Validation is already running" });
       return;
     } else {
-      const { libPath } = req.body;
-
-      const newValidation = await validationModel.create();
-      res.json(newValidation);
+      const newValidation = await validationModel.create(
+        "pending",
+        req.body.libPath,
+      );
+      res.status(202).json(newValidation);
 
       job = spawn(
-        "node",
-        ["../../jobs/validate.js", libPath],
-        VALIDATION_JOB_OPTS,
+        NODE_PROCESS,
+        [VALIDATION_SCRIPT_PATH, req.body.libPath],
+        VALIDATION_OPTS,
       )
-        .on("message", async (message) => {
-          console.log("*** MESSAGE", message);
-          await validationModel.update(
-            newValidation.validationId,
-            JSON.parse(message.toString()),
-          );
+        .on("message", async (message: ValidationResult) => {
+          await validationModel.update(newValidation.validationId, message);
         })
-        .on("close", async () => (job = null));
-      res.status(202);
+        .on("close", () => (job = null))
+        .on("error", async (err) => {
+          console.log("[error in child process (validation job)]", err);
+          await validationModel.destroy(newValidation.validationId);
+        });
       return;
     }
   } catch (err) {
@@ -53,7 +58,6 @@ export async function getValidation(
   next: NextFunction,
 ) {
   try {
-    console.log("+++");
     res.json(await validationModel.read(req.params.id));
   } catch (err) {
     next(err);
@@ -61,20 +65,24 @@ export async function getValidation(
 }
 
 export async function cancelValidation(
-  req: Request<any, any, any, { id: number }>,
+  req: Request<{ id: number }>,
   res: Response,
   next: NextFunction,
 ) {
   try {
+    console.log(job);
     if (!job || !job.pid) {
-      res.status(404);
-      return;
-    }
+      throw new HttpError({
+        code: 404,
+        message: `There is no running validation job with ID ${req.params.id}`,
+      });
+    } else {
+      job.kill("SIGTERM");
 
-    job.kill("SIGTERM");
-    job = null;
-    await validationModel.destroy(req.params.id);
-    res.status(200);
+      job = null;
+      await validationModel.destroy(req.params.id);
+      res.status(200).end();
+    }
   } catch (err) {
     next(err);
   }
