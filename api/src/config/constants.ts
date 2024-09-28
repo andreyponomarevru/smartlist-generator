452da6b4a229d61bc66aaf18d40e605fb7986e5b
Andrey Ponomarev <info@andreyponomarev.ru>
2024-09-28 15:27:34 +0300

    refactor: Move genres to JSON file

--- api/src/config/constants.ts
+++ api/src/config/constants.ts
@@ -1,3 +1,8 @@
+import genresJSON from "./genres.json";
+
+export type Genre = { id: number; name: string };
+export const GENRES: ReadonlyArray<Genre> = genresJSON;
+
 export const API_PREFIX = "/api";
 
 //
@@ -13,40 +18,3 @@ export const FILTER_CONDITIONS = [
   "does not contain all",
   "does not contain any",
 ] as const;
-
-export type Genre = { id: number; name: string };
-export const GENRES: ReadonlyArray<Genre> = [
-  { id: 0, name: "[Soundtrack]" },
-  { id: 1, name: "2-Step" },
-  { id: 2, name: "Acid House" },
-  { id: 3, name: "Ambient" },
-  { id: 4, name: "Balearic" },
-  { id: 5, name: "Beatless" },
-  { id: 6, name: "Breakbeat" },
-  { id: 7, name: "Closer" },
-  { id: 8, name: "Disco" },
-  { id: 9, name: "Downtempo" },
-  { id: 10, name: "Dub" },
-  { id: 11, name: "Electro" },
-  { id: 12, name: "Garage House" },
-  { id: 13, name: "Hip-Hop" },
-  { id: 14, name: "House" },
-  { id: 15, name: "Italo House" },
-  { id: 16, name: "Jungle" },
-  { id: 17, name: "Latin" },
-  { id: 18, name: "Opener" },
-  { id: 19, name: "Pop" },
-  { id: 20, name: "Pop House" },
-  { id: 21, name: "Psytrance" },
-  { id: 22, name: "R&B" },
-  { id: 23, name: "Rock" },
-  { id: 24, name: "Soul" },
-  { id: 25, name: "Techno" },
-  { id: 26, name: "Trance" },
-  { id: 27, name: "Trip-Hop" },
-  { id: 28, name: "UK Garage" },
-  { id: 29, name: "Vocal" },
-  { id: 30, name: "World Music" },
-  { id: 31, name: "Field Recording" },
-  { id: 32, name: "Spoken Word" },
-];
